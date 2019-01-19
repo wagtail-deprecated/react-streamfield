0a4509b2d5ba45fa2ae8c8e97af58fefb0f64725
Thibaud Colas <thibaudcolas@gmail.com>
2019-01-19 13:50:33 +0200

    Switch to Rollup for library build

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/index.js",
+    external: []
+      .concat(Object.keys(pkg.dependencies))
+      .concat(Object.keys(pkg.peerDependencies)),
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+    ],
+    plugins: [
+      babel({
+        exclude: ["node_modules/**"],
+      }),
+    ],
+  },
+];
